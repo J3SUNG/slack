6fc653a3463db9f8da3e8eb77aaf6a93b2f9ae7a
wptjd6141@naver.com <wptjd6141@naver.com>
2024-04-09 06:49:07 +0900

    Feat: 워크스페이스 구현
    - 로그아웃
    - 프로필 열기
    - 프로필 닫기
    - 워크스페이스 생성 창 열기
    - 워크스페이스 생성 창 닫기
    - 워크스페이스 생성 요청
    - 스타일 적용
    - 레이아웃 설정

--- layouts/App/index.tsx
+++ layouts/App/index.tsx
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 const Login = loadable(() => import('@pages/Login'));
 const SignUp = loadable(() => import('@pages/SignUp'));
+const Workspace = loadable(() => import('@layouts/Workspace'));
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Redirect exact path="/" to="/login" />
       <Route path="/login" component={Login} />
       <Route path="/signUp" component={SignUp} />
+      <Route path="/workspace" component={Workspace} />
     </Switch>
   );
 };
