1abb9cb9bc1a227db6080552c69ee8bb67938945
wptjd6141@naver.com <wptjd6141@naver.com>
2024-04-25 04:18:57 +0900

    Feat: channel style 적용

---
+++ pages/Channel/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Header } from './style';
+
+const Channel = () => {
+  return (
+    <Container>
+      <Header>채널!</Header>
+    </Container>
+  );
+};
+
+export default Channel;
