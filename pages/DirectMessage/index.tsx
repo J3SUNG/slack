aa94d1139154fded0d3cc9b9141e066e52bf7581
wptjd6141@naver.com <wptjd6141@naver.com>
2024-04-27 23:18:33 +0900

    컴포넌트 구현

---
+++ pages/DirectMessage/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Header } from './styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+
+const DirectMessage = () => {
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR<IUser>('/api/users', fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat="" />
+    </Container>
+  );
+};
+
+export default DirectMessage;
