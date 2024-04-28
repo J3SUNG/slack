c4618f4a47474f0098e2d3a70b6cb02d24d3c7c2
wptjd6141@naver.com <wptjd6141@naver.com>
2024-04-28 23:38:05 +0900

    Feat: 채팅 리스트 기본 컴포넌트 생성

---
+++ components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import { ChatZone, Section } from './styles';
+import React from 'react';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section>ChatList</Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
