50148d3ea2c2315f8c01f0567cf80125fae75014
wptjd6141@naver.com <wptjd6141@naver.com>
2024-04-30 15:22:25 +0900

    Feat: DM 구현

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
-import { IUser } from '@typings/db';
+import { IDM, IUser } from '@typings/db';
 import fetcher from '@utils/fetcher';
 import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
 import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
   const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher);
   const { data: myData } = useSWR<IUser>('/api/users', fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
+
+  const onSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (chat?.trim()) {
+        axios
+          .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+            content: chat,
+          })
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch(console.error);
+      }
+    },
+    [chat],
+  );
 
   if (!userData || !myData) {
     return null;
@@ -23,8 +48,8 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList />
-      <ChatBox chat="" />
+      <ChatList chatData={chatData} />
+      <ChatBox chat={chat} onSubmitForm={onSubmitForm} onChangeChat={onChangeChat} />
     </Container>
   );
 };
